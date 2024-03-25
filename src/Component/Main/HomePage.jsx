bbc3f3b7e311dc806f4167e93923a1c795065e74
AH-sunny <sunnycse03@gmail.com>
2024-03-25 16:41:52 +0600

    note: footer add

--- src/Component/Main/HomePage.jsx
+++ src/Component/Main/HomePage.jsx
@@ -1,26 +1,32 @@
 import { Outlet, useNavigation } from "react-router-dom";
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 
 const Main = () => {
-const navigation = useNavigation();
+    const navigation = useNavigation();
 
 
 
     return (
-        <div className="container mx-auto text-center ">
-            <h1 className="text-4xl font-bold mb-9">Handeling react route</h1>
-            <Header></Header>
-            {
-                navigation.state === 'loading' ?
-                <span className="loading loading-bars loading-lg text-primary"> </span>
-                : 
-                <Outlet></Outlet>
-            }
-           
-            {/* <Footer></Footer> */}
+        <div >
+            <div className="container mx-auto text-center min-h-[318px]">
+                <h1 className="text-4xl font-bold mb-9">Handeling react route</h1>
+                <Header></Header>
+                {
+                    navigation.state === 'loading' ?
+                        <span className="loading loading-bars loading-lg text-primary"> </span>
+                        :
+                        <Outlet></Outlet>
+                }
+            </div>
 
-            
+            <div className="mt-5 w-full">
+
+                <Footer></Footer>
+            </div>
+
+
         </div>
     );
 };
